1eea48f60a4d645cab831a5930197f3a01e5146b
choviZ <702381764@qq.com>
2025-08-18 15:38:50 +0800

    feat:后端项目初始化

---
+++ cp-backend/src/main/java/com/zcw/cpbackend/CpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zcw.cpbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CpBackendApplication.class, args);
+    }
+
+}
